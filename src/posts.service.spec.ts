259e65789237ef954b3faaa57dd1bf8a602e30b3
Suganov Boris <suganov@t8.ru>
2024-12-28 17:49:54 +0300

    finish task

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,24 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
+  it("should add a new post", () => {
+    const created_post = postsService.create(post);
+    expect(postsService.find(created_post.id)).toEqual(created_post);
   });
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
+  it("should find a post", () => {
+    const created_post = postsService.create(post);
+    expect(postsService.find(created_post.id)).toEqual(created_post);
   });
 });
